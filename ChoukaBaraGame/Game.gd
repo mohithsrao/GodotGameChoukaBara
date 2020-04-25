7898860ef2e614434907366133d18f1df7db9884
mohithsrao <mohithsrao@gmail.com>
2020-04-25 16:27:43 +0530

    Updated Navigation and Mesh for all players

--- ChoukaBaraGame/Game.gd
+++ ChoukaBaraGame/Game.gd
@@ -3,20 +3,38 @@ extends Node2D
 var selectedCharacter
 var tile_size : = 192
 
-onready var playerSpawnerInstance : = $PlayerSpawner
+onready var playerSpawnerOneInstance : = $PlayerSpawnerOne
+onready var playerSpawnerTwoInstance : = $PlayerSpawnerTwo
+onready var playerSpawnerThreeInstance : = $PlayerSpawnerThree
+onready var playerSpawnerFourInstance : = $PlayerSpawnerFour
 
 func _ready() -> void:
-	for character in playerSpawnerInstance.get_children():
+	for character in playerSpawnerOneInstance.get_children():
+		character.connect("character_selected",self,"_on_character_selected")
+		character.connect("character_unselected",self,"_on_character_unselected")
+	for character in playerSpawnerTwoInstance.get_children():
+		character.connect("character_selected",self,"_on_character_selected")
+		character.connect("character_unselected",self,"_on_character_unselected")
+	for character in playerSpawnerThreeInstance.get_children():
+		character.connect("character_selected",self,"_on_character_selected")
+		character.connect("character_unselected",self,"_on_character_unselected")
+	for character in playerSpawnerFourInstance.get_children():
 		character.connect("character_selected",self,"_on_character_selected")
 		character.connect("character_unselected",self,"_on_character_unselected")
 
 func _unhandled_input(event : InputEvent) -> void:
 	if event is InputEventMouseButton and selectedCharacter != null:
 		if event.button_index == BUTTON_LEFT and event.pressed:
-			var path = $Navigation2D.get_simple_path(selectedCharacter.position, event.position)
+			var navigationInstance = getNavigationInstanceforSelectedCharactor(selectedCharacter)
+			var path = navigationInstance.get_simple_path(selectedCharacter.position, event.position)
 			var normalizedPath = normalizeNavigationPath(path)
 			selectedCharacter.navigationPath = normalizedPath
 			$Line2D.points = normalizedPath
+
+func getNavigationInstanceforSelectedCharactor(selectedCharacter):
+	var navigationInstance = selectedCharacter.get_node("../Navigation2D")
+	return navigationInstance
+
 
 func normalizeNavigationPath(path:PoolVector2Array) -> PoolVector2Array:
 	var resultArray = PoolVector2Array()
