8812cc5390d15b10748a9eeaf515e8b9967b9131
mohithsrao <mohithsrao@gmail.com>
2020-07-07 21:54:59 +0530

    Re-Roll pawn working

--- ChoukaBaraGame/Scripts/StateMachine/GameStateMachine/SelectPawn.gd
+++ ChoukaBaraGame/Scripts/StateMachine/GameStateMachine/SelectPawn.gd
@@ -6,14 +6,15 @@ func enter(owner):
 		if(PlayerInfo.active_player && PlayerInfo.active_player.needsReRoll):
 			PlayerInfo.active_player.needsReRoll = false
 			emit_signal("finished","CalculateGara")
-		emit_signal("finished","SelectNextPlayer")
+		else:
+			emit_signal("finished","SelectNextPlayer")
 	else:
 		yield(PlayerInfo.active_player,"pawnSelected")
-		emit_signal("finished","MovePawn")
 		PlayerInfo.active_player.selectedPawn.call_deferred("enableHitBox",true)
+		emit_signal("finished","MovePawn")
 
-func update(_delta):
-	#	When player hits a enemy pawn
-	if(PlayerInfo.active_player && PlayerInfo.active_player.needsReRoll):
-		emit_signal("finished","CalculateGara")
-		PlayerInfo.active_player.needsReRoll = false
+#func update(_delta):
+#	#	When player hits a enemy pawn
+#	if(PlayerInfo.active_player && PlayerInfo.active_player.needsReRoll):
+#		emit_signal("finished","CalculateGara")
+#		PlayerInfo.active_player.needsReRoll = false
